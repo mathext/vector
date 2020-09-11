adef1c94e2c0ed9e9ba1ec1f1b050af2d8e3af5b
Franklin Chieze <chieze.franklin@gmail.com>
2020-09-11 12:42:42 +0100

    Create type declaration file

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,12 @@
 export declare class Vector {
-  // google how to create dec files
+  public constructor(x: number, y: number, z: number);
+  public add(vector: Vector): Vector;
+  public divide(scalar: number): Vector
+  public multiply(scalar: number): Vector;
+  public subtract(vector: Vector): Vector;
+  public to2D(): number[];
+  public to3D(): number[];
   public get x(): number;
+  public get y(): number;
+  public get z(): number;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export default class Vector {
-  // write test for class
+  // Constructors
   public constructor(x: number = 0, y: number = 0, z: number = 0) {
     this._x = x;
     this._y = y;
