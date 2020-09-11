53fc66517df0c4de7b5d36953b135a82ebdc9f12
Franklin Chieze <chieze.franklin@gmail.com>
2020-09-11 11:56:37 +0100

    Setup Package

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,4 @@
-export declare class Vector { // google how to create dec files
-    public get x(): number;
+export declare class Vector {
+  // google how to create dec files
+  public get x(): number;
 }
