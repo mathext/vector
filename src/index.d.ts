32eb5a97138faf36d32c411700acf666fa05715e
Franklin Chieze <chieze.franklin@gmail.com>
2020-09-11 12:43:16 +0100

    Patch version to 0.3

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,7 @@
 export declare class Vector {
   public constructor(x: number, y: number, z: number);
   public add(vector: Vector): Vector;
-  public divide(scalar: number): Vector
+  public divide(scalar: number): Vector;
   public multiply(scalar: number): Vector;
   public subtract(vector: Vector): Vector;
   public to2D(): number[];
